5b05c9ec9a6a49db40ee21a392ababb0c7217400
Aidah Novallia Putri <aidahptr15@gmail.com>
2025-03-20 14:53:45 +0700

    feat: modify level 2 transition tilemap

--- scenes/scripts/StartButton.gd
+++ scenes/scripts/StartButton.gd
@@ -10,4 +10,4 @@ func _on_pressed():
 	call_deferred("start_1_scene")
 
 func start_1_scene():
-	get_tree().change_scene_to_file(str("res://scenes/levels/Level1.tscn"))
+	get_tree().change_scene_to_file(str("res://scenes/levels/Level2Transition.tscn"))
