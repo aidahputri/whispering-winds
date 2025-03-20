cfef7c3edaa6e025965210c7a48c8520d8f4db66
Aidah Novallia Putri <aidahptr15@gmail.com>
2025-03-20 17:23:09 +0700

    feat: fix player layer

--- scenes/scripts/StartButton.gd
+++ scenes/scripts/StartButton.gd
@@ -11,3 +11,4 @@ func _on_pressed():
 
 func start_1_scene():
 	get_tree().change_scene_to_file(str("res://scenes/levels/Level2Transition.tscn"))
+	#get_tree().change_scene_to_file(str("res://scenes/levels/Level1.tscn"))
