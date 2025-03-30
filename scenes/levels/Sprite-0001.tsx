a8e2d9c70624235e8d1cc9ca98db381ca0624ba3
Aidah Novallia Putri <aidahptr15@gmail.com>
2025-03-30 16:49:00 +0700

    feat: init level 2 tilemap

---
+++ scenes/levels/Sprite-0001.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.0" name="Sprite-0001" tilewidth="16" tileheight="16" tilecount="180" columns="15">
+ <image source="Sprite-0001.png" width="240" height="192"/>
+</tileset>
