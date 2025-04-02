f02a030613391309578d05bfaa37cc9585ca47dd
Aidah Novallia Putri <aidahptr15@gmail.com>
2025-04-02 19:55:57 +0700

    feat: modify level 2

--- scenes/scripts/Epilogue.gd
+++ scenes/scripts/Epilogue.gd
@@ -6,8 +6,9 @@ extends Node2D
 @onready var title_label = $CanvasLayer/Label
 @onready var fade_timer = $CanvasLayer/FadeTimer
 
+signal text_change
+
 var fading_in = true
-var fading_out = false
 var fade_speed = 1.5
 
 var epilog_text = [
@@ -18,7 +19,7 @@ var epilog_text = [
 ]
 
 var current_text_index = 0
-var text_change_timer = 1.0 # Change text every 1 second
+var text_change_timer = 3.5
 
 func _ready():
 	background.modulate.a = 0
@@ -26,13 +27,10 @@ func _ready():
 
 	fade_timer.wait_time = 2.0
 	fade_timer.one_shot = true
-	fade_timer.timeout.connect(_on_fade_timer_timeout) 
 
-	# Start the fade-in effect
 	fading_in = true
 	fade_timer.start()
 
-	# Wait for the fade-in to complete, then start the epilogue text
 	await get_tree().create_timer(2.0).timeout
 	start_epilogue_text()
 
@@ -45,33 +43,23 @@ func _process(delta):
 			fading_in = false
 			fade_timer.start()
 
-	elif fading_out:
-		background.modulate.a = max(background.modulate.a - fade_speed * delta, 0)
-		title_label.modulate.a = max(title_label.modulate.a - fade_speed * delta, 0)
-
-		if background.modulate.a <= 0:
-			change_scene()
-
 func _on_fade_timer_timeout():
-	fade_out()
-
-func fade_out():
-	fading_out = true
+	emit_signal("text_change")
 
 func change_scene():
-	get_tree().change_scene_to_file("res://scenes/levels/" + target_scene + ".tscn")
+	get_tree().change_scene_to_file("res://scenes/cutscenes/TitleScreen.tscn")
 
-# Start updating the epilogue text at regular intervals
 func start_epilogue_text():
 	var timer = get_tree().create_timer(text_change_timer)
-	timer.timeout.connect(self, "_on_text_change_timeout")
+	timer.timeout.connect(self._on_text_change_timeout)
 
 func _on_text_change_timeout():
 	if current_text_index < epilog_text.size():
 		title_label.text = epilog_text[current_text_index]
 		current_text_index += 1
+		emit_signal("text_change")
+		
+		var timer = get_tree().create_timer(text_change_timer)
+		timer.timeout.connect(self._on_text_change_timeout)
 	else:
-		# After showing "The End", stop the fade out
-		fading_out = false
-		# Optional: If you want to trigger the scene change after the text is fully shown, uncomment this line
-		# change_scene()
+		change_scene()
--- scenes/scripts/StartButton.gd
+++ scenes/scripts/StartButton.gd
@@ -1,16 +1,18 @@
 extends Button
 
-#func _on_mouse_entered():
-	#border.visible = true
-#
-#func _on_mouse_exited():
-	#border.visible = false
-	
 func _on_pressed():
 	call_deferred("start_1_scene")
 
 func start_1_scene():
 	#get_tree().change_scene_to_file(str("res://scenes/levels/Level2Transition.tscn"))
-	get_tree().change_scene_to_file(str("res://scenes/levels/Level2.tscn"))
-	#get_tree().change_scene_to_file(str("res://scenes/cutscenes/Start1.tscn"))
+	#get_tree().change_scene_to_file(str("res://scenes/levels/Level2.tscn"))
+	get_tree().change_scene_to_file(str("res://scenes/cutscenes/Start1.tscn"))
+	#get_tree().change_scene_to_file(str("res://scenes/cutscenes/InTheRuin.tscn"))
+	#get_tree().change_scene_to_file(str("res://scenes/cutscenes/Epilogue.tscn"))
 	#get_tree().change_scene_to_file(str("res://scenes/levels/Level1.tscn"))
+
+func _on_exit_pressed():
+	call_deferred("exit_game")
+	
+func exit_game():
+	get_tree().quit()
